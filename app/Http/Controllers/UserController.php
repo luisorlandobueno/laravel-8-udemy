04a3e609829c90e61eca1c66d1918ede7f6f97f1
luis orlando muñoz bueno <105979241+luisorlandobueno@users.noreply.github.com>
2023-08-11 16:58:13 -0500

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,24 +1,44 @@
-<?php
+<?php 
 
 namespace App\Http\Controllers;
 
+
 use Illuminate\Http\Request;
 
-class UserController extends Controller
-{
-    public function index()
-    {
+
+
+class  UserController extends Controller{
+
+
+
+    public function index(){
+
         return view('welcome');
+
     }
 
-    public function showName($name)
-    {
-        return view('example', ['name' => $name]);
+
+
+    public function showname($name){
+
+
+        return view('example' , ['name' => $name]);
+
+
     }
 
-    public function suma($num = 0)
-    {
-        $b = 77;
-        return $num + $b;
+
+    public function suma($num = 0){
+
+        $b = 80;
+        return $num +$b;
+
+
     }
+
+
+
+
+
+
 }
