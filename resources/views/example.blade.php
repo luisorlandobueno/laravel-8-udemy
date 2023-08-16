6f8e6b4e0456f576c1cb1b59d275e3c682a99782
luis orlando muñoz bueno <105979241+luisorlandobueno@users.noreply.github.com>
2023-08-16 18:30:56 -0500

    Update example.blade.php

--- resources/views/example.blade.php
+++ resources/views/example.blade.php
@@ -1,1 +1,18 @@
 
+@extends('layouts.appp')
+
+@section('title' , 'este es el titulo')
+
+
+<h1> hola {{$name}} bienvenido</h1>
+
+
+@section('content')
+
+tu nombre {{$name}}<br>
+tu apellido {{$apellido}}<br>
+tu edad {{$edad}}<br>
+
+
+
+@endsection
