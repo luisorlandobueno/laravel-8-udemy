3eaff8e013e65096062f87f3391c5732225e8e53
luis orlando muñoz bueno <105979241+luisorlandobueno@users.noreply.github.com>
2023-08-16 18:30:02 -0500

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,1 +1,60 @@
 
+<?php 
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Http\Request;
+
+
+
+class  UserController extends Controller{
+
+
+
+   public function __construct(){
+
+
+       // $this->middleware('checkage');
+      //  $this->middleware('checkage')->only('index' , 'showname');
+       // $this->middleware('checkage')->except('suma');
+
+
+   }
+
+
+    public function index(){
+
+        $name="luis";
+        $apellido="bueno";
+        $edad=33;
+
+        return view('example',['name' => $name , 'apellido'=>$apellido, 'edad'=>$edad]);
+
+    }
+
+
+
+    public function showname($name){
+
+
+        return view('example' , ['name' => $name]);
+
+
+    }
+
+
+    public function suma($num = 0){
+
+        $b = 80;
+        return $num +$b;
+
+
+    }
+
+
+
+
+
+
+}
